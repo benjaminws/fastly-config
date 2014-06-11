8b712ae563472b1ccf0b786b7b1fccdb9dc15738
Ezekiel Templin <zeke@templ.in>
2014-06-10 18:10:59 -0700

    Add deploy to CLI

--- lib/fastly/configure/cli.rb
+++ lib/fastly/configure/cli.rb
@@ -13,6 +13,18 @@ class Fastly
                      output_directory: output }
         CDN.new(settings).build
       end
+
+      desc 'deploy [PATH TO YAML] [SERVICE NAME] [USER] [PASS] [OUTPUT DIRECTORY]',
+        'Builds a new VCL and deploys Fastly service configuration'
+      def deploy(path_to_yaml, service_name, user, password, output = './')
+        settings = { settings_file: path_to_yaml,
+                     service_name: service_name,
+                     operation: 'fastly',
+                     user: user,
+                     pass: password,
+                     output_directory: output }
+        CDN.new(settings).build
+      end
     end
   end
 end
